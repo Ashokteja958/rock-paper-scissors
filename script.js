09b8b62a9d25356da89a3847bb4ba54c936f82a1
ashok teja <aashokteja958@gmail.com>
2025-12-31 10:42:33 +0530

    Add UI for Rock Paper Scissors

--- script.js
+++ script.js
@@ -1,40 +1,44 @@
-game = ["rock","paper","scissors"]
+const game = ["rock", "paper", "scissors"]
+
 let humanscore = 0
 let computerscore = 0
-function playRound(humanChoice, computerChoice) {
-    humanChoice = prompt("Enter Your Choice:", "rock").toLowerCase();
-    let x = Math.floor(Math.random()*3)
-    computerChoice = game[x].toLowerCase();
+let rounds = 0
 
+function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
-        console.log(`It's a tie! Both chose ${humanChoice}`);
-        return;
-        }
+        return `It's a tie! Both chose ${humanChoice}`
+    }
 
     if (
         (humanChoice === "rock" && computerChoice === "scissors") ||
         (humanChoice === "paper" && computerChoice === "rock") ||
         (humanChoice === "scissors" && computerChoice === "paper")
-        ) {
-        humanscore++;
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-        return humanscore
+    ) {
+        humanscore++
+        return `You win! ${humanChoice} beats ${computerChoice}`
     } else {
-        computerscore++;
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-        return computerscore
+        computerscore++
+        return `You lose! ${computerChoice} beats ${humanChoice}`
     }
 }
 
+const buttons = document.querySelectorAll("[data-choice]")
 
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (rounds === 5) return
 
-function playGame(){    
-    for(let i = 0; i<5;i++){
-        playRound()
-    }
-    if(humanscore>computerscore) console.log("you won")
-    else if(humanscore === computerscore) console.log("its a tie")
-    else console.log("you lose")
-}
+        const humanChoice = button.dataset.choice.toLowerCase()
+        const computerChoice = game[Math.floor(Math.random() * 3)]
 
-playGame()
+        console.log(playRound(humanChoice, computerChoice))
+        rounds++
+
+        if (rounds === 5) {
+            if (humanscore > computerscore) console.log("You won the game")
+            else if (humanscore < computerscore) console.log("You lost the game")
+            else console.log("The game is a tie")
+        }
+    })
+})
+
